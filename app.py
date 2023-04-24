d9ba5623bd13e33ad6db926b21aea54066bb280d
Hai Nguyen Dang <haind@sonat.vn>
2023-04-24 20:43:45 +0700

    add white, black list

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 import traceback
 from flask_cors import CORS, cross_origin
 import sklearn
+from urllib.parse import urlparse
 # from preprocessing import detect
 from preprocessing_v2 import detect_phishing
 
@@ -14,17 +15,24 @@ from preprocessing_v2 import detect_phishing
 app = Flask(__name__)
 try:
     model = joblib.load('RFC_model.pkl')
-    print('Model loaded')
 except Exception as e:
-    print('Model loading error')
     print(str(e))
 cors = CORS(app)
+
+with open('whitelist.txt') as f:
+    whitelist = f.read().splitlines()
+
+print(whitelist)
+
+with open('blacklist.txt') as f:
+    blacklist = f.read().splitlines()
+
+print(blacklist)
+
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/getprediction', methods=['GET', 'POST'])
-
-
 @cross_origin()
 def getprediction():
     if model:
@@ -32,14 +40,20 @@ def getprediction():
         try:
             # get url from request query params
             url = request.args.get('url')
-            print(url)
-            features = detect_phishing(url)
-            print(features)
-            prediction = model.predict([features])
-            print("prediction: ", prediction)
-            return jsonify({'prediction': str(prediction)})
+            domain = urlparse(url).netloc
+            #check domain is in whitelist
+            if domain in whitelist:
+                return jsonify({'prediction': '[0]'})
+            elif domain in blacklist:
+                return jsonify({'prediction': '[1]'})
+            else:
+                features = detect_phishing(url)
+                print(features)
+                prediction = model.predict([features])
+                print("prediction: ", prediction)
+                return jsonify({'prediction': str(prediction)})
         except:
             return jsonify({'trace': traceback.format_exc()})
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    app.run(threaded=True)
