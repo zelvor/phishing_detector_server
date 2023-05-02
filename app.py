76f27de8e3b28e89971e1f9f7382da43f0478a55
Hai Nguyen Dang <haind@sonat.vn>
2023-05-02 09:35:39 +0700

    update

--- app.py
+++ app.py
@@ -9,8 +9,14 @@ import sklearn
 from tld import get_tld
 # from preprocessing import detect
 from preprocessing_v2 import detect_phishing
+from azure.storage.blob import BlobServiceClient, ContentSettings, BlobType
+from azure.core.exceptions import AzureError
 
-
+# Azure Blob Storage credentials
+connect_str = 'DefaultEndpointsProtocol=https;AccountName=phishingdetectorstorage;AccountKey=6tIBbQBjw0fFxkrkJVfiFGbOwIBY5iL3kq0oaH2kQTCHyEJPa4OWuj3gy1vL/3VKLaRlJnPMiLo++ASt6gr8hg==;EndpointSuffix=core.windows.net'
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+container_client = blob_service_client.get_container_client('list')
+blob_client = container_client.get_blob_client('report_url.txt')
 
 app = Flask(__name__)
 try:
@@ -21,13 +27,12 @@ cors = CORS(app)
 
 with open('whitelist.txt') as f:
     whitelist = f.read().splitlines()
-# print(whitelist)
 
 with open('blacklist.txt') as f:
     blacklist = f.read().splitlines()
-# print(blacklist)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/getprediction', methods=['GET'])
 @cross_origin()
@@ -38,16 +43,14 @@ def getprediction():
             # get url from request query params
             url = request.args.get('url')
             domain = get_tld(url, as_object=True).fld
-            #check domain is in whitelist
+            # check domain is in whitelist
             if domain in whitelist:
                 return jsonify({'prediction': '[0]'})
             elif domain in blacklist:
                 return jsonify({'prediction': '[1]'})
             else:
                 features = detect_phishing(url)
-                print(features)
                 prediction = model.predict([features])
-                print("prediction: ", prediction)
                 return jsonify({'prediction': str(prediction)})
         except:
             return jsonify({'trace': traceback.format_exc()})
@@ -57,11 +60,17 @@ def getprediction():
 @cross_origin()
 def postreporturl():
     url = request.args.get('url')
-    print(url)
-    # write url to report_url.txt
-    with open('report_url.txt', 'a') as f:
-        f.write(url + '\n')
-    return jsonify({'message': 'success'})
+    # send url to report_url.txt on Azure Blob Storage
+    try:
+        report_url_content = blob_client.download_blob().readall().decode('utf-8')
+        report_url_list = report_url_content.splitlines()
+        if url not in report_url_list:
+            blob_client.upload_blob(url + '\n', blob_type=BlobType.AppendBlob, overwrite=False,
+                                    content_settings=ContentSettings(content_type='text/plain'))
+        return jsonify({'message': 'success'})
+    except AzureError as e:
+        return jsonify({'message': 'failed'})
+
 
 if __name__ == "__main__":
     app.run(threaded=True)
